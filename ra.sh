06bb64de7d5b2a32aa757cc7970eb64f9cfc9090
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-11-05 12:33:22 -0800

    added feedback to ra.sh

--- ra.sh
+++ ra.sh
@@ -20,3 +20,8 @@ bash invoke.sh apis/"$API" "$CMD"
 done
 }
 done
+echo "
+       Copyright (c) 2019 Michael Neill Hartman. All rights reserved.
+       mnh_license@proton.me
+       https://github.com/hartmanm
+"
